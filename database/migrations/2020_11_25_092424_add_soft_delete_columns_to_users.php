320bc94a8b260d62084af99da7357f997704278b
Vladislavs Buzinskis <intelifoxgames@gmail.com>
2020-11-25 12:05:05 +0200

    Update

--- database/migrations/2020_11_25_092424_add_soft_delete_columns_to_users.php
+++ database/migrations/2020_11_25_092424_add_soft_delete_columns_to_users.php
@@ -6,27 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 class AddSoftDeleteColumnsToUsers extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::table('articles', function (Blueprint $table) {
             $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::table('articles', function (Blueprint $table) {
-            //
+            $table->dropSoftDeletes();
         });
     }
 }
